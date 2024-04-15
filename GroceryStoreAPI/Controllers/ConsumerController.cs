16723247c4120852bf44dd77dfcf8a0d3c97459b
Harishtechginie <harish.maurya@techginie.in>
2024-04-15 14:18:29 +0530

    push all code on github

---
+++ GroceryStoreAPI/Controllers/ConsumerController.cs
@@ -0,0 +1,80 @@
+﻿using GroceryStoreAPI.DataAccess;
+using GroceryStoreAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace GroceryStoreAPI.Controllers
+{
+    [Route("api/Consumer")]
+    [ApiController]
+    public class ConsumerController : Controller
+    {
+        private readonly ILogger<ConsumerController> _logger;
+        private readonly IDataAccessProvider _dataAccessProvider;
+
+        public ConsumerController(ILogger<ConsumerController> logger, IDataAccessProvider dataAccessProvider)
+        {
+            _logger = logger;
+            _dataAccessProvider = dataAccessProvider;
+        }
+//        {“task_name”: “Buy Grocery”, “tags”: [“household”, “weekly stuff”],
+//“due_date”: "2024-04-09", “color”: “beige”,
+//“assigned_to”: “bala”, “status”: “PENDING”}
+
+
+    [HttpGet("GetConsumerRecord")]
+        public IEnumerable<Consumer> Get()
+        {
+            return _dataAccessProvider.GetConsumerRecords();
+        }
+
+        [HttpPost("AddConsumerRecord")]
+        public IActionResult Create([FromBody] Consumer Consumer)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid obj = Guid.NewGuid();
+                Consumer.id = obj.ToString();
+                _dataAccessProvider.AddConsumerRecord(Consumer);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("GetConsumerById/{id}")]
+        public Consumer Details(string id)
+        {
+            return _dataAccessProvider.GetConsumerSingleRecord(id);
+        }
+
+        [HttpGet("GetConsumerSearch/{task_name}/{tag}/{due_date}/{status}")]
+        public List<Consumer> Search(string task_name, string tag, DateTime due_date, string status)
+        {
+            return _dataAccessProvider.GetConsumerSearch(task_name,tag,due_date,status);
+        }
+
+        [HttpPut("UpdateConsumerRecord")]
+        public IActionResult Edit([FromBody] Consumer Consumer)
+        {
+            if (ModelState.IsValid)
+            {
+                _dataAccessProvider.UpdateConsumerRecord(Consumer);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("DeleteConsumerRecord/{id}")]
+        public IActionResult DeleteConfirmed(string id)
+        {
+            var data = _dataAccessProvider.GetConsumerSingleRecord(id);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            _dataAccessProvider.DeleteConsumerRecord(id);
+            return Ok();
+        }
+    }
+}
---
+++ GroceryStoreAPI/DataAccess/DataAccessProvider.cs
@@ -0,0 +1,52 @@
+﻿using GroceryStoreAPI.Model;
+using System.Threading;
+
+namespace GroceryStoreAPI.DataAccess
+{
+    public class DataAccessProvider : IDataAccessProvider
+    {
+        private readonly PostgreSqlContext _context;
+
+        public DataAccessProvider(PostgreSqlContext context)
+        {
+            _context = context;
+        }
+
+        public void AddConsumerRecord(Consumer consumer)
+        {
+            _context.consumers.Add(consumer);
+            _context.SaveChanges();
+        }
+
+        public void UpdateConsumerRecord(Consumer consumer)
+        {
+            _context.consumers.Update(consumer);
+            _context.SaveChanges();
+        }
+
+        public void DeleteConsumerRecord(int id)
+        {
+            var entity = _context.consumers.FirstOrDefault(t => t.id == id);
+            _context.consumers.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public Consumer GetConsumerSingleRecord(int id)
+        {
+            return _context.consumers.FirstOrDefault(t => t.id == id);
+        }
+
+        public List<Consumer> GetConsumerSearch(string task_name, string tag, DateTime due_date, string status)
+        {
+            return _context.consumers.Where(p => p.task_record.task_name == task_name
+                                                        && p.task_record.tags.tagName == tag
+                                                        && p.task_record.due_date == due_date
+                                                        && p.task_record.status == status).ToList();
+        }
+
+        public List<Consumer> GetConsumerRecords()
+        {
+            return _context.consumers.ToList();
+        }
+    }
+}
